18487cac96791791a508220113c5405dfc309a65
Luis Fernando <luisfernando20140@outlook.com>
2020-06-25 14:32:03 -0300

    Projeto iniciado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AulaDia25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
